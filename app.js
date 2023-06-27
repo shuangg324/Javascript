2d0550a58005709f3b8b6157b907e4f4fbb93508
Sean <hsean324@gmail.com>
2023-06-27 02:11:09 -0700

    progress

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 for (let i = 1; i <= 20; i++){
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("Frontend Simplified"); 
+        console.log(`${i} -> Frontend Simplified`); 
     }
     else if (i % 5 == 0) {
-        console.log("Simplified");
+        console.log(`${i} -> Simplified`);
     }
     else if (i % 3 == 0) {
-        console.log("Frontend");
+        console.log(`${i} -> Frontend`);
     }
     else{
-        console.log(i);
+        console.log(`${i} -> ${i}`);
     }
     
 }
