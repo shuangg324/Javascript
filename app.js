e6e6126e68db8b62724dde8755dd690c395dc9ac
Sean <hsean324@gmail.com>
2023-06-27 02:18:04 -0700

    finished loops

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-for (let i = 1; i <= 20; i++){
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log(`${i} -> Frontend Simplified`); 
-    }
-    else if (i % 5 == 0) {
-        console.log(`${i} -> Simplified`);
-    }
-    else if (i % 3 == 0) {
-        console.log(`${i} -> Frontend`);
-    }
-    else{
-        console.log(`${i} -> ${i}`);
-    }
-    
+let x = "Frontend Simplified"
+
+for (i = 0; i < x.length; i++) {
+    console.log(x[i])
 }
