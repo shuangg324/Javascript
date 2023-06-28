87c43d0bdecb109f814a7a9808427dc97136a30a
Sean <hsean324@gmail.com>
2023-06-27 21:37:23 -0700

    2 ways of defining function

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-let x = "Frontend Simplified"
+// function convertCtoF(celcius){
+//     return celsius * 1.8 + 32
+// }
 
-for (i = 0; i < x.length; i++) {
-    console.log(x[i])
+//another way of defining a function using arrow
+const convertCtoF = (celsius) => {
+    return celsius * 1.8 + 32
 }
+
+console.log(convertCtoF(0));
+console.log(convertCtoF(10));
+console.log(convertCtoF(30));
